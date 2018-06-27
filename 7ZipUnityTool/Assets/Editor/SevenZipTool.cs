e87dbd016d2812f3b7af0dc76ab828e35ac6047c
fangyi4828 <float4828@163.com>
2018-06-27 17:26:22 +0800

    auto input file patjh

--- 7ZipUnityTool/Assets/Editor/SevenZipTool.cs
+++ 7ZipUnityTool/Assets/Editor/SevenZipTool.cs
@@ -14,10 +14,14 @@ public class SevenZipTool : EditorWindow {
     private string url = "Please Input File Path Which Need Compress";
     private void OnGUI()
     {
-        url = EditorGUILayout.TextField(url);
-        if(GUILayout.Button("Compress"))
+        UnityEngine.Object[] arr = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.TopLevel);
+        url = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf('/')) + "/" + AssetDatabase.GetAssetPath(arr[0]);
+        EditorGUILayout.TextField(url);
+        
+        if (GUILayout.Button("Compress"))
         {
             SevenZipTask.StartCompress(new string[] { url}, "");
         }
+        this.Repaint();
     }
 }
