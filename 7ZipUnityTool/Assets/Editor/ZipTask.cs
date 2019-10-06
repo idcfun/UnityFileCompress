9a8fcc66c39a80a3050f09b3a0c937ea1a8c8bff
fange <fange@fangedeMacBook-Pro.local>
2019-10-06 16:44:51 +0800

    recode

---
+++ 7ZipUnityTool/Assets/Editor/ZipTask.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class ZipTask {
+
+    
+    public static void StartCompress(string url, string[] inputPaths, string outputPath)
+    {
+        ZipTask task = new ZipTask();
+        task.Compress(url, inputPaths, outputPath);
+    }
+
+    public static void StartDecompress(string decompressfile)
+    {
+        ZipTask task = new ZipTask();
+        task.Decompress(decompressfile);
+    }
+
+    private void Compress(string url, string[] inputPaths, string outputPath)
+    {
+        SevenZip.Compression.LZMA.Encoder coder = new SevenZip.Compression.LZMA.Encoder();
+        FileStream output = new FileStream(outputPath, FileMode.Create);
+        //Write the encoder properties
+        coder.WriteCoderProperties(output);
+        FileStream input;
+        int fileCount = inputPaths.Length;
+        byte[] bytes = BitConverter.GetBytes(fileCount);
+        output.Write(bytes, 0, 4);
+        for (int i = 0; i < fileCount; i++)
+        {
+            string path = inputPaths[i];
+            input = new FileStream(path, FileMode.Open);
+            long inputStreamLen = input.Length;
+
+            ///Write the file name 
+            string filename = path.Substring(url.Length+1, path.Length-url.Length-1);
+            Debug.Log(filename);
+
+            byte[] nameBytes = Encoding.Default.GetBytes(filename);
+            output.Write(BitConverter.GetBytes(nameBytes.Length), 0, 4);
+            output.Write(nameBytes, 0, nameBytes.Length);
+
+            // Write the file size.
+            bytes = BitConverter.GetBytes(inputStreamLen);
+            output.Write(bytes, 0, 8);
+
+            // Encode the file.
+
+            coder.Code(input, output, inputStreamLen, -1, null);
+            output.Flush();
+            input.Close();
+        }
+
+        output.Close();
+    }
+
+    private void Decompress(string inputPath)
+    {
+        SevenZip.Compression.LZMA.Decoder coder = new SevenZip.Compression.LZMA.Decoder();
+        FileStream input = new FileStream(inputPath, FileMode.Open);
+
+        input.Position = 0;
+        // Read the decoder properties
+        byte[] properties = new byte[5];
+        input.Read(properties, 0, 5);
+        coder.SetDecoderProperties(properties);
+        byte[] bytes = new byte[4];
+        input.Read(bytes, 0, 4);
+        int fileCount = BitConverter.ToInt32(bytes, 0);
+
+        for(int i = 0; i < fileCount; i++)
+        {
+            bytes = new byte[4];
+            input.Read(bytes, 0, 4);
+            int nameLength = BitConverter.ToInt32(bytes, 0);
+            bytes = new byte[nameLength];
+            input.Read(bytes, 0, nameLength);
+            string name = Encoding.Default.GetString(bytes);
+            string dir = Application.streamingAssetsPath + "/"+Path.GetDirectoryName(name);
+
+            if(!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+                Debug.Log(dir);
+            }
+
+            bytes = new byte[8];
+            input.Read(bytes, 0, 8);
+            long fileSize = BitConverter.ToInt64(bytes, 0);
+            FileStream output = new FileStream(Application.streamingAssetsPath + "/" + name, FileMode.OpenOrCreate);
+            coder.Code(input, output, input.Length, fileSize, null);
+            output.Flush();
+            output.Close();
+        }
+
+        input.Close();
+    }
+}
---
+++ 7ZipUnityTool/Assets/Editor/ZipTool.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+
+public class ZipTool : EditorWindow {
+
+    [MenuItem("Tools/ZipTool")]
+    public static void Compress()
+    {
+        CreateInstance<ZipTool>().Show();
+    }
+
+    private string url = "Please Input File Path Which Need Compress";
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Compress"))
+        {
+            url = EditorUtility.OpenFolderPanel("test", "", "");
+            string[] files = Directory.GetFiles(url, "*.cs", SearchOption.AllDirectories);
+            ZipTask.StartCompress(url, files, Application.streamingAssetsPath + "/" + Path.GetFileName(url) + ".zip");
+            AssetDatabase.Refresh();
+        }
+
+        if(GUILayout.Button("Uncompress"))
+        {
+            url = EditorUtility.OpenFilePanel("test", "", "");
+            ZipTask.StartDecompress(url);
+            AssetDatabase.Refresh();
+        }
+
+        this.Repaint();
+    }
+}
